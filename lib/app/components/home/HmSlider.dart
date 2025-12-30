8fd1287432332c4cd00b719864f7227a49a3d68d
平板-胡勇强 <262489227@qq.com>
2025-12-30 10:58:01 +0800

    轮播图的搜索框和指示灯导航

--- lib/app/components/home/HmSlider.dart
+++ lib/app/components/home/HmSlider.dart
@@ -12,10 +12,21 @@ class HmSlider extends StatefulWidget {
 }
 
 class _HmSliderState extends State<HmSlider> {
+  int _currentIndex = 0;
+
+  // 轮播图切换时更新当前索引
+  void _updateCurrentIndex(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  CarouselSliderController _controller = CarouselSliderController();
+
   @override
   Widget build(BuildContext context) {
     // Stack -> 轮播图 + 搜索框 指示灯导航
-    return Stack(children: [_getSlider()]);
+    return Stack(children: [_getSlider(), _getSearch(), _getDots()]);
     // return Container(
     //   height: 200,
     //   color: Colors.blue,
@@ -24,11 +35,70 @@ class _HmSliderState extends State<HmSlider> {
     // );
   }
 
+  Widget _getSearch() {
+    return Positioned(
+      top: 10,
+      left: 0,
+      right: 0,
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Container(
+          alignment: Alignment.centerLeft,
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          height: 50,
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(0, 0, 0, 0.4),
+            borderRadius: BorderRadius.circular(25),
+          ),
+          child: Text(
+            "搜索...",
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // 返回指示灯导航部件
+  Widget _getDots() {
+    return Positioned(
+      left: 0,
+      right: 0,
+      bottom: 10,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: List.generate(widget.bannerList.length, (index) {
+          return GestureDetector(
+            onTap: () {
+              _controller.animateToPage(index);
+              _updateCurrentIndex(index);
+            },
+            child:
+            // Container(
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
+              width: index == _currentIndex ? 40 : 20,
+              height: 6,
+              margin: EdgeInsets.symmetric(horizontal: 4),
+              decoration: BoxDecoration(
+                color: index == _currentIndex
+                    ? Colors.white
+                    : Color.fromRGBO(0, 0, 0, 0.3),
+                borderRadius: BorderRadius.circular(3),
+              ),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+
   Widget _getSlider() {
     // 在flutter中获取屏幕宽度的方法
     final screenWidth = MediaQuery.of(context).size.width;
 
     return CarouselSlider(
+      carouselController: _controller,
       items: List.generate(widget.bannerList.length, (index) {
         return Image.network(
           widget.bannerList[index].imgUrl!,
@@ -42,6 +112,9 @@ class _HmSliderState extends State<HmSlider> {
         autoPlay: true,
         enlargeCenterPage: true,
         viewportFraction: 0.8,
+        onPageChanged: (index, reason) {
+          _updateCurrentIndex(index);
+        },
       ),
     );
   }
--- lib/app/view_models/home_models.dart
+++ lib/app/view_models/home_models.dart
@@ -1,7 +1,7 @@
 // 每一个轮播图的具体类型
 class BannerItem {
-  String? id;
-  String? imgUrl;
+  String id;
+  String imgUrl;
 
   BannerItem({required this.id, required this.imgUrl});
 }
